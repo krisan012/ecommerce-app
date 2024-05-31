57d36a3873f7b4a53c9d9b278f807c868185d776
MarkT <matuazon43@gmail.com>
2024-06-01 04:14:30 +0800

    Added route middleware and roles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::middleware(['role:Administrator'])->group(function () {
+//        Route::resource('users', UserController::class);
+    });
+
+    Route::resource('products', ProductController::class);
+//    Route::resource('categories', CategoryController::class);
 });
 
 require __DIR__.'/auth.php';
